164726152ffdddf36cd40c235369a2e783d22e72
Rachel Li <rmli@cs.uchicago.edu>
2021-02-18 14:19:50 -0600

    added covid dataframes

--- dataframes.py
+++ dataframes.py
@@ -4,7 +4,7 @@ from functools import reduce
 def create_dataframes(variable_list):
     '''
     variable_list (list): list of strings
-    options for variables are: "broadband-subscriptions", "child-mortality", "co2", "dalys",
+    optios for variables are: "broadband-subscriptions", "child-mortality", "co2", "dalys",
     "dependency", "drug-deaths", "expected-schooling", "female-labor", "fertility", "gov-expenditure",
     "happiness", "homicides", "life-expectancy", "pollution-deaths", "savings"]
     variable_list has a maximum of three variables
@@ -17,5 +17,17 @@ def create_dataframes(variable_list):
     merged = reduce(lambda left, right: pd.merge(left,right,on=['Code','Year', 'Entity']), df_list)
 
     return merged
+
+def read_covid_data(variable_list):
+    '''
+    possible variables: new_cases_per_million, new_deaths_per_million, weekly_icu_patients_per_million, new_tests_per_thousand, new_vaccinations_smoothed_per_million
+    '''
+    url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
+    df = pd.read_csv(url,index_col=0,parse_dates=[0])
+
+    interest = df[variable_list]
+
+
+
 
 
