5ceb02cfc612dd349d302996fe19594c01b540cf
Samantha Koretsky <skoretsky@cs.uchicago.edu>
2021-03-08 13:15:45 -0600

    use subprocess library to get r code into python and use the results. still need to write the actual r script, and then test whether this python code actually works bc subprocess seems weird

--- regression.py
+++ regression.py
@@ -3,6 +3,7 @@ import numpy as np
 import dataframes
 import statsmodels.api as stat
 import statsmodels.formula.api as stat_f
+import subprocess
 codes = ['AFG', 'ALB', 'DZA', 'AND', 'AGO', 'AIA', 'ATG', 'ARG', 'ARM', 'ABW', 'AUS', 'AUT', 'AZE', 'BHS', 'BHR', 'BGD', 'BRB', 'BLR', 'BEL', 'BLZ', 'BEN', 'BTN', 'BOL', 'BIH', 'BWA', 'BRA', 'BRN', 'BGR', 'BFA', 'BDI', 'KHM', 'CMR', 'CAN', 'CPV', 'CAF', 'TCD', 'CHL', 'CHM', 'COL', 'COM', 'COG', 'COK', 'CRI', 'CIV', 'HRV', 'CUB', 'CUW', 'CYP', 'CZE', 'COD', 'DNK', 'DJI', 'DMA', 'DOM', 'ECU', 'EGY', 'SLV', 'GNQ', 'ERI', 'EST', 'SWZ', 'ETH', 'FJI', 'FIN', 'FRA', 'GUF', 'GAB', 'GMB', 'GEO', 'DEU', 'GHA', 'GRC', 'GRD', 'GUM', 'GTM', 'GIN', 'GNM', 'GUY', 'HTI', 'HND', 'HKG', 'HUN', 'ISL', 'IND', 'IDN', 'IRN', 'IRQ', 'IRL', 'ISR', 'ITA', 'JAM', 'JPN', 'JOR', 'KAZ', 'KEN', 'KIR', 'KWT', 'KGZ', 'LAO', 'LVA', 'LBN', 'LSO', 'LBR', 'LBY', 'LTU', 'LUX', 'MAC', 'MDG', 'MWI', 'MYS', 'MDV', 'MLI', 'MLT', 'MHL', 'MTQ', 'MRT', 'MUS', 'MYT', 'MEX', 'FSM', 'MDA', 'MCO', 'MNG', 'MNE', 'MAR', 'MOZ', 'MMR', 'NAM', 'NRU', 'NPL', 'NLD', 'NCL', 'NZL', 'NIC', 'NER', 'NGA', 'NIU', 'PRK', 'MKD', 'OMN', 'PAK', 'PLW', 'PSE', 'PAN', 'PNG', 'PRY', 'PER', 'PHL', 'POL', 'PRT', 'PRI', 'QAT', 'ROU', 'RUS', 'RWA', 'KNA', 'LCA', 'VCT', 'WSM', 'SMR', 'STP', 'SAU', 'SEN', 'SRB', 'SYC', 'SLE', 'SGP', 'SVK', 'SVN', 'SLB', 'SOM', 'ZAF', 'KOR', 'SSD', 'ESP', 'LKA', 'SDN', 'SUR', 'CHE', 'SYR', 'TJK', 'TZA', 'TJK', 'THA', 'TLS', 'TGO', 'TON', 'TTO', 'TUN', 'TUR', 'TKM', 'TUV', 'UGA', 'UKR', 'ARE', 'GBR', 'USA', 'UZB', 'VUT', 'VEN', 'VNM', 'YEM', 'ZMB', 'ZWE']
 
 
@@ -31,3 +32,26 @@ def get_model(x_var,y_var):
     #het = het_test[1] < 0.05
 
     return model
+    
+
+def compute_r(x_var,y_var):
+    '''
+    Does some kind of R code using the subprocess library 
+    (https://docs.python.org/3/library/subprocess.html) and writes the results
+    back to python
+    '''
+    # see https://www.kdnuggets.com/2015/10/integrating-python-r-executing-part2.html?fbclid=IwAR3IL1c51hFzVLWKI_i-Bd0UohaOnUym1guYGxjkKl0zR66nUDDy90xB2vo
+    var_list = [x_var, y_var]
+    
+    # blah blah R code - need to actually write an R script
+    command ='Rscript'
+    path2script ='path/to your script/script.R'
+    
+    # check_output will run the command and store to result
+    # write the r script so that result will be a bunch of text that tells us
+    # things we want (regression values, etc)
+    cmd = [command, path2script] + var_list
+    #result = subprocess.check_output(cmd, universal_newlines=True)
+    result = subprocess.run(cmd, capture_output=True)
+    
+    return result
