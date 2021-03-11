a06c24b08c10aed74f485170858049ec42f4101c
Rachel Li <rmli@cs.uchicago.edu>
2021-03-11 16:30:13 -0600

    style and comments

--- dataframes.py
+++ dataframes.py
@@ -1,13 +1,21 @@
 import pandas as pd
 from functools import reduce
 
+'''
+This file deals with cleaning and creating elements of dataframes.
+Citations:
+    https://stackoverflow.com/questions/44327999/python-pandas-merge-multiple-dataframes
+    for how to merge multiple dataframes
+'''
+
 def create_dataframes(variable_list, countries):
     '''
-    variable_list (list): list of strings
-    options for variables are: "broadband-subscriptions", "child-mortality", "co2", "dalys",
-    "dependency", "drug-deaths", "expected-schooling", "female-labor", "fertility", "gov-expenditure",
-    "happiness", "homicides", "life-expectancy", "pollution-deaths", "savings"]
-    variable_list has a maximum of three variables
+    Creates a merged dataframe for the variables and countries of interest
+    Inputs:
+        variable_list (list): list of strings
+        countries (list): list of country codes
+    Ouputs:
+        merged (dataframe)
     '''
     
     df_list = []
@@ -20,14 +28,19 @@ def create_dataframes(variable_list, countries):
 
     return merged
 
-
 def clean_column_name(variable):
     '''
     This cleans the column name for the variable description
     We want to keep the string in parentheses if it tells us something
-        about the units, for example "Age dependency ratio (% of working-age population)"
+        about the units, for example 
+        "Age dependency ratio (% of working-age population)"
         but we want to discard the string in parentheses if it relates to 
-        the data source, for example "Public expenditure on health %GDP (OWID extrapolated series)"
+        the data source, for example 
+        "Public expenditure on health %GDP (OWID extrapolated series)"
+    Input:
+        variable (str): column name for the variable of interest
+    Ouput:
+        cleaned column name
     '''
 
     inside = variable[variable.find("(")+1:variable.find(")")]
@@ -35,6 +48,3 @@ def clean_column_name(variable):
         return variable
     else:
         return variable[0: variable.find("(")]
-
-
-
