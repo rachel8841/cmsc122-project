6e8527930f2b8703f373e5fc7019429ca3ae98bd
Rachel Li <rmli@cs.uchicago.edu>
2021-03-11 16:45:42 -0600

    style and comments

--- webscraping.py
+++ webscraping.py
@@ -1,26 +1,35 @@
 import bs4
 import requests
 
+'''
+File for webscraping information about the variable from ourworldindata.org
+'''
+
 def scrape(var_list):
     '''
-    returns tuple with three strings
+    Input:
+        var_list (list): list of variable name strings
+    Ouput:
+        tuple with three strings
     '''
     x,y,bub = var_list
 
     return (get_description(x), get_description(y), get_description(bub))
 
-
 def get_description(variable):
     '''
     Gets the description of one variable
+    Input:
+        variable (str)
+    Ouput:
+        description (str)
     '''
     url = "https://ourworldindata.org/grapher/" + variable
     soup = get_soup(url)
     description_tag = soup.find("meta", attrs={'name':"description"})
     description = description_tag["content"]
     
-    return description
-    
+    return description  
 
 def get_soup(url):
     '''
